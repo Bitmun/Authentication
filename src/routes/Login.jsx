b363894eacbd897c2544c7961f68bd414f55a52e
unknown <pashanbalko21@gmail.com>
2022-11-08 19:47:58 +0300

    16.20 - video

--- src/routes/Login.jsx
+++ src/routes/Login.jsx
@@ -1,5 +1,21 @@
+import { useCallback, useState } from "react"
 function Login() {
-  return <div>Login</div>
+  const [email, setEmail] = useState("")
+  const handleSetEmail = useCallback((e) => setEmail(e.target.value), [])
+  const [password, setPassword] = useState("")
+  const handleSetPassword = useCallback((e) => setPassword(e.target.value), [])
+
+  const handleLogin = useCallback(() => {
+    localStorage.setItem("user", JSON.stringify({ email, password }))
+  }, [email, password])
+  return (
+    <div className=" flex flex-col items-center gap-2">
+      <input placeholder="email" onChange={handleSetEmail} value={email}></input>
+
+      <input type="password" placeholder="password" onChange={handleSetPassword} value={password}></input>
+      <button onClick={handleLogin}>Log in</button>
+    </div>
+  )
 }
 
 export default Login
